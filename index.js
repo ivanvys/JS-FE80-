52fdd69d7138f1bec487920e9648c792c57c99d9
ivanvys <ivanvysv8@gmail.com>
2022-05-12 21:33:30 +0300

    Function

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ let b=x(3,10);
 console.log (q);
 console.log (b);*/
 
-function isIven (value) {
-ddd
-}
+const x = function (value) {
+if (value>10) {
+return 'error, number can not be grater than 11'
+} if (value>5) {
+return'it is middle'
+} if (value>1) {
+return'OK'
+} return 'лох'};
+
+console.log(x(5));
