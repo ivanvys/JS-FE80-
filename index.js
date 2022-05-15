433edcd26325d37c42add49b3073f1d7ad5fd981
ivanvys <ivanvysv8@gmail.com>
2022-05-15 19:01:33 +0300

    new

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 
 function sayHello (name) {
 if (name.toLowerCase()==="mark") {
-return 'Hi, '+name.charAt(0).toUpperCase()+name.slice(1)}+"!";
-return 'Hello, '+name.charAt(0).toUpperCase()+name.slice(1)+"!"
+return 'Hi, '+name.charAt(0).toUpperCase()+name.slice(1).toLowerCase()+"!"};
+return 'Hello, '+name.charAt(0).toUpperCase()+name.slice(1).toLowerCase()+"!"
 };
 
 const sayHello=(name2)=> {
 if (name2.toLowerCase()==="mark") {
-return 'Hi, '+name2.charAt(0).toUpperCase()+name2.slice(1)+"!"};
-return 'Hello, '+name2.charAt(0).toUpperCase()+name2.slice(1)+"!"
+return 'Hi, '+name2.charAt(0).toUpperCase()+name2.slice(1).toLowerCase()+"!"};
+return 'Hello, '+name2.charAt(0).toUpperCase()+name2.slice(1).toLowerCase()+"!"
 };
 
 /*Второе задание*/
