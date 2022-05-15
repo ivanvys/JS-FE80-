711fd5f7b660f4150247233c125090189c530f2b
ivanvys <ivanvysv8@gmail.com>
2022-05-16 00:50:32 +0300

    Domashka 1

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 
 /* Первое задание*/
 
-function sayHello (name) {
+const sayHello=(name)=> {
 if (name.toLowerCase()==="mark") {
 return 'Hi, '+name.charAt(0).toUpperCase()+name.slice(1).toLowerCase()+"!"};
-return 'Hello, '+name.charAt(0).toUpperCase()+name.slice(1).toLowerCase()+"!"
-};
-
-const sayHello=(name2)=> {
-if (name2.toLowerCase()==="mark") {
-return 'Hi, '+name2.charAt(0).toUpperCase()+name2.slice(1).toLowerCase()+"!"};
-return 'Hello, '+name2.charAt(0).toUpperCase()+name2.slice(1).toLowerCase()+"!"
+return 'Hello, '+name.charAt(0).toUpperCase()+name.slice(1).toLowerCase()+"!";
 };
 
 /*Второе задание*/
 
+const triangle=(m,n)=> {
+return Math.sqrt((m*m)+(n*n));
+};
+
+/*Третье задание*/
+
+const lowerNumber = (a,b)=> {
+if (a>b) {
+return b};
+return a;
+};
+
+/*Четвертое задание*/
+
+const isEven =(n)=> {
+if (n%2===0) {
+return true};
+return false
+};
+
+/*Пятое задание*/
+
+const deleteChars=(str)=>{
+return str.slice(1,-1);
+};
+
+/*Шестое задание*/
+const SomeFn=(value)=> {
+return value.charAt(0).toUpperCase()+value.slice(1).toLowerCase()
+};
+
+/*Седьмое задание*/
+const checkLetterCase=(value)=> {
+if (value.charAt(0).toUpperCase()==value.charAt(0)) {
+return 'Оууу май, большая буква!'};
+return 'Нет уж, маленькие буквы - скучно'
+};
+
+/*Восьмое задание*/
+
+const concatenate =(a,b)=> {
+return a+" "+b;
+}
+
+/*Девятое задание*/
+
+const checkStringLength =(a,b)=> {
+if (a.length>=b) {
+return "String is too long!"};
+return a;
+}
