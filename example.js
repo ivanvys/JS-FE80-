5d537919c883b7f469d0407443c43aafe6a649fe
ivanvys <ivanvysv8@gmail.com>
2022-06-11 12:38:32 +0300

    new

--- example.js
+++ example.js
@@ -1,46 +1,17 @@
-class PokeApi {
-  BASE_URL = "https://pokeapi.co/api/v2/pokemon";
-
-  async getPokemonsDetailsWithConvertedStats() {
-    try {
-      const pokemons = await this.getPokemons();
-
-      const pokemonDetails = await this.getPokemonDetails(pokemons);
-
-      return pokemonDetails.map((pokemon) => ({
-        ...pokemon,
-        stats: this.statsConverter(pokemon.stats),
-      }));
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-
-  async getPokemons() {
-    const { results } = await fetch(this.BASE_URL).then((response) =>
-      response.json()
-    );
-
-    return results;
-  }
-
-  async getPokemonDetails(pokemons) {
-    const pokemonDetailsCalls = pokemons.map(({ url }) =>
-      fetch(url).then((response) => response.json())
-    );
-
-    return Promise.all(pokemonDetailsCalls);
-  }
-
-  statsConverter(pokemonDetailedList) {
-    return pokemonDetailedList.reduce((statMap, { base_stat, stat }) => {
-      statMap[stat.name] = base_stat;
-
-      return statMap;
-    }, {});
-  }
-}
-
-const api = new PokeApi();
-
-api.getPokemonsDetailsWithConvertedStats().then((data) => console.log(data));
+const y = undefined;
+const g = (value) => {
+  const e = [...value];
+  const q = e
+    .reduce((acc, element) => {
+      if (!acc.includes(element)) {
+        acc.push(element);
+      }
+      return acc;
+    }, [])
+    .join("");
+  return value.toLowerCase() === q.toLowerCase() ||
+    value.toLowerCase() === undefined
+    ? "true"
+    : "false";
+};
+console.log(g(y));
--- useful JS.js
+++ useful JS.js
@@ -104,3 +104,48 @@ const e = async () => {
   });
   console.log(g);
 };
+
+class PokeApi {
+  BASE_URL = "https://pokeapi.co/api/v2/pokemon";
+
+  async getPokemonsDetailsWithConvertedStats() {
+    try {
+      const pokemons = await this.getPokemons();
+
+      const pokemonDetails = await this.getPokemonDetails(pokemons);
+
+      return pokemonDetails.map((pokemon) => ({
+        ...pokemon,
+        stats: this.statsConverter(pokemon.stats),
+      }));
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async getPokemons() {
+    const { results } = await fetch(this.BASE_URL).then((response) =>
+      response.json()
+    );
+
+    return results;
+  }
+
+  async getPokemonDetails(pokemons) {
+    const pokemonDetailsCalls = pokemons.map(({ url }) =>
+      fetch(url).then((response) => response.json())
+    );
+
+    return Promise.all(pokemonDetailsCalls);
+  }
+
+  statsConverter(pokemonDetailedList) {
+    return pokemonDetailedList.reduce((statMap, { base_stat, stat }) => {
+      statMap[stat.name] = base_stat;
+
+      return statMap;
+    }, {});
+  }
+}
+
+const api = new PokeApi();
